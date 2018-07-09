9e2d086f59dfcce2e12db0bc6607ea6efdc57357
Alexey Zelenin <alexey@zelenin.org>
2018-07-10 00:48:28 +0300

    Turned off MatrixSnapshotsService

--- src/Lykke.Service.ArbitrageDetector/Controllers/ArbitrageDetectorController.cs
+++ src/Lykke.Service.ArbitrageDetector/Controllers/ArbitrageDetectorController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Lykke.Service.ArbitrageDetector.Aspects.ExceptionHandling;
-using Lykke.Service.ArbitrageDetector.Core.Domain.Interfaces;
 using Lykke.Service.ArbitrageDetector.Core.Services;
 using Lykke.Service.ArbitrageDetector.Models;
 using Microsoft.AspNetCore.Mvc;
