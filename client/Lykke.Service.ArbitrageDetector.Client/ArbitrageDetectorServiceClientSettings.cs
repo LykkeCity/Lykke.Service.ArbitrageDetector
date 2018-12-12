74e3266cc106fe756980e50e0d9f875dedba432a
Alexey Zelenin <alexey@zelenin.org>
2018-12-12 11:57:34 +0300

    Updated RabbitMqBroker

--- client/Lykke.Service.ArbitrageDetector.Client/ArbitrageDetectorServiceClientSettings.cs
+++ client/Lykke.Service.ArbitrageDetector.Client/ArbitrageDetectorServiceClientSettings.cs
@@ -2,14 +2,22 @@
 
 namespace Lykke.Service.ArbitrageDetector.Client.Models 
 {
+    /// <summary>
+    /// Arbitrage Detector Client Settings.
+    /// </summary>
     public class ArbitrageDetectorServiceClientSettings
     {
+        /// <summary>
+        /// Service url.
+        /// </summary>
         public string ServiceUrl { get; set; }
 
+        /// <inheritdoc />
         public ArbitrageDetectorServiceClientSettings()
         {
         }
 
+        /// <inheritdoc />
         public ArbitrageDetectorServiceClientSettings(string serviceUrl)
         {
             ServiceUrl = string.IsNullOrWhiteSpace(serviceUrl) ? throw new ArgumentNullException(nameof(serviceUrl)) : serviceUrl;
